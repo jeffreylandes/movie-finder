29a49a00b345f1c3513ab4970a79ca0922e15cbe
Jeffrey <jeffrey.landes@autodesk.com>
2022-01-03 20:49:27 -0700

    refactor: movie new movie popularity to own file

---
+++ src/pages/newMovies/MoviePopularityComponent.tsx
@@ -0,0 +1,77 @@
+import { Actor } from "../../Movies/fetchActors";
+import { ActorAndMovies, SlimMovie } from "../../Movies/fetchNewMovies";
+import { getFullUrlFromPoster } from "../../Movies/Movies";
+
+type MovieAndActor = {
+  movie: SlimMovie;
+  actor: Actor;
+};
+
+type MovieAndActors = {
+  movie: SlimMovie;
+  actors: Actor[];
+};
+
+function ActorImage(actor: Actor) {
+  return (
+    <img
+      src={getFullUrlFromPoster(actor.profilePath)}
+      width={"300px"}
+      height={"300px"}
+    ></img>
+  );
+}
+
+function MovieAndActorsComponent(movieAndActors: MovieAndActors) {
+  const actors = movieAndActors.actors.map((actor) => ActorImage(actor));
+  return (
+    <div>
+      <img
+        src={getFullUrlFromPoster(movieAndActors.movie.posterPath)}
+        width={"300px"}
+        height={"300px"}
+      ></img>
+      {actors}
+    </div>
+  );
+}
+
+function sortByMoviePopularity(
+  movieOne: MovieAndActors,
+  movieTwo: MovieAndActors
+) {
+  return movieOne.movie.popularity >= movieTwo.movie.popularity ? -1 : 1;
+}
+
+export function MoviePopularityComponent(newMovies: ActorAndMovies[]) {
+  const flatMovies: MovieAndActor[] = newMovies.flatMap(
+    (actorAndMovies: ActorAndMovies) =>
+      actorAndMovies.movies.map((movie: SlimMovie) => ({
+        movie: movie,
+        actor: actorAndMovies.actor,
+      }))
+  );
+  const movieToActorsMap = new Map<string, MovieAndActors>();
+  flatMovies.forEach((movieAndActor: MovieAndActor) => {
+    const movieId = movieAndActor.movie.movieId;
+    const actors = movieToActorsMap.get(movieId);
+    const newActors =
+      actors !== undefined
+        ? actors.actors.concat([movieAndActor.actor])
+        : [movieAndActor.actor];
+    const movieAndActors: MovieAndActors = {
+      movie: movieAndActor.movie,
+      actors: newActors,
+    };
+    movieToActorsMap.set(movieId, movieAndActors);
+  });
+  const movieToActors = Array.from(movieToActorsMap.values()).sort(
+    (movieOne, movieTwo) => sortByMoviePopularity(movieOne, movieTwo)
+  );
+  const components = movieToActors.map((movieAndActors) =>
+    MovieAndActorsComponent(movieAndActors)
+  );
+  return (
+    <div style={{ width: "85%", backgroundColor: "gray" }}>{components}</div>
+  );
+}
--- src/pages/newMovies/NewMoviePage.tsx
+++ src/pages/newMovies/NewMoviePage.tsx
@@ -1,66 +1,8 @@
 import { useRecoilValue, useRecoilValueLoadable } from "recoil";
-import { Actor } from "../../Movies/fetchActors";
-import { ActorAndMovies, SlimMovie } from "../../Movies/fetchNewMovies";
-import { getFullUrlFromPoster } from "../../Movies/Movies";
+import { ActorAndMovies } from "../../Movies/fetchNewMovies";
 import { newFavoriteMovies } from "../../Movies/state";
+import { MoviePopularityComponent } from "./MoviePopularityComponent";
 import { displayTypeAtom, Navbar } from "./NavbarNewMovies";
-
-type MovieAndActor = {
-  movie: SlimMovie;
-  actor: Actor;
-};
-
-type MovieAndActors = {
-    movie: SlimMovie;
-    actors: Actor[];
-}
-
-function ActorImage(actor: Actor) {
-    return <img src={getFullUrlFromPoster(actor.profilePath)} width={"300px"} height={"300px"}></img>
-}
-
-function MovieAndActorsComponent(movieAndActors: MovieAndActors) {
-    const actors = movieAndActors.actors.map((actor) => ActorImage(actor));
-    return (
-        <div>
-            <img src={getFullUrlFromPoster(movieAndActors.movie.posterPath)} width={"300px"} height={"300px"}></img>
-            {actors}
-        </div>
-    )
-}
-
-function sortByMoviePopularity(movieOne: MovieAndActors, movieTwo: MovieAndActors) {
-    return movieOne.movie.popularity >= movieTwo.movie.popularity ? -1 : 1;
-}
-
-function MoviePopularityComponent(newMovies: ActorAndMovies[]) {
-  const flatMovies: MovieAndActor[] = newMovies.flatMap(
-    (actorAndMovies: ActorAndMovies) =>
-      actorAndMovies.movies.map((movie: SlimMovie) => ({
-        movie: movie,
-        actor: actorAndMovies.actor,
-      }))
-  );
-  const movieToActorsMap = new Map<string, MovieAndActors>();
-  flatMovies.forEach(
-      (movieAndActor: MovieAndActor) => {
-        const movieId = movieAndActor.movie.movieId;
-        const actors = movieToActorsMap.get(movieId);
-        const newActors = actors !== undefined ? actors.actors.concat([movieAndActor.actor]) : [movieAndActor.actor];
-        const movieAndActors: MovieAndActors = {movie: movieAndActor.movie, actors: newActors};
-        movieToActorsMap.set(movieId, movieAndActors);
-      }
-  )
-  const movieToActors = Array.from(movieToActorsMap.values()).sort((movieOne, movieTwo) => sortByMoviePopularity(movieOne, movieTwo));
-  const components = movieToActors.map((movieAndActors) => 
-    MovieAndActorsComponent(movieAndActors)
-  );
-  return (
-      <div style={{"width": "85%", "backgroundColor": "gray"}}>
-          {components}
-      </div>
-  )
-}
 
 function ActorComponent(newMovies: ActorAndMovies[]) {
   return <></>;
